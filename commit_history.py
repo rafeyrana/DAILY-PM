4542728bf46c8982b488818c4eea1761b2b435c9
Rafey Rana <24100103@lums.edu.pk>
2024-09-01 00:32:37 +0500

    added parser

--- commit_history.py
+++ commit_history.py
@@ -1,4 +1,21 @@
 import requests
+from datetime import datetime
+
+class Commit:
+    def __init__(self, commit_data):
+        self.sha = commit_data['sha']
+        self.author_name = commit_data['commit']['author']['name']
+        self.author_email = commit_data['commit']['author']['email']
+        self.date = datetime.strptime(commit_data['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")
+        self.message = commit_data['commit']['message']
+        self.html_url = commit_data['html_url']
+
+    def __str__(self):
+        return f"Commit: {self.sha[:7]}\n" \
+               f"Author: {self.author_name} <{self.author_email}>\n" \
+               f"Date: {self.date.strftime('%Y-%m-%d %H:%M:%S')}\n" \
+               f"Message: {self.message}\n" \
+               f"URL: {self.html_url}\n"
 
 def get_commit_history(owner, repo):
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
@@ -22,8 +39,16 @@ def get_commit_history(owner, repo):
 
     return commits
 
-# Example usage
+def parse_commits(commit_history):
+    return [Commit(commit) for commit in commit_history]
+
+def print_formatted_commits(commits):
+    for commit in commits:
+        print(commit)
+        print("-" * 50)
+
 owner = "rafeyrana"
 repo = "DAILY-PM"
 commit_history = get_commit_history(owner, repo)
-print(commit_history)
+parsed_commits = parse_commits(commit_history)
+print_formatted_commits(parsed_commits)
